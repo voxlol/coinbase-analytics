e456476c6e83af323958141bbed5d54c19adc618
voxlol <voxlol@outlook.com>
2015-08-29 16:35:34 -0500

    Created coinbase.js for coinbase connecitons

---
+++ server/coinbase.js
@@ -0,0 +1,9 @@
+var coinbase = require('coinbase-exchange');
+
+
+// Web Socket Connections
+// var websocket = new coinbase.WebsocketClient();
+
+// websocket.on('message', function(data){
+//   console.log(data);
+// });
