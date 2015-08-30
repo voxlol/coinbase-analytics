28ff8b1e7622de0babb5a795286282527df50d95
voxlol <voxlol@outlook.com>
2015-08-30 13:37:16 -0500

    Grunt file now compiling all coffeescript to 1 target js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,54 @@
 // Grunt configuration file
 module.exports = function(grunt){
   grunt.initConfig({
+    // JS Tasks
+    coffee: {
+      build: {  // Build
+        // expand: true,
+        // cwd: 'client',
+        // src: ['**/*.coffee'],
+        // dest: 'dist',
+        // ext: '.js'
+        files: {
+          'client/js/app.js' : ['client/**/*.coffee', 'client/*.coffee']
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        files:{
+          'dist/app.min.js' : ['dist/**/*.js', 'dist/*.js']
+        }
+      }
+    },
+
+    // // Convert Less to css
+    // less: {
+    //   dist: { 
+    //     files: {
+    //       'client/styles.css' : ['dist/style.less']
+    //     }
+    //   }
+    // },
+
+    // // Minify the CSS
+    // cssmin: {
+    //   build: { 
+    //     files: {
+    //       'dist/style.min.css' : ['dist/**/*.css', 'dist/*.css']
+    //     }
+    //   }
+    // },
+
+    // Watch Configuration
+    watch: {
+      coffee: {
+        files: ['client/**/*.coffee', 'client/*.coffee'],
+        tasks: ['coffee']
+      }
+    },
+
     // nodemon configuration
     nodemon: {
       dev: {
@@ -11,7 +59,12 @@ module.exports = function(grunt){
 
   // Load Tasks
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Register the tasks to default
-  grunt.registerTask('default', ['nodemon']);
+  grunt.registerTask('default', ['coffee', 'uglify', 'less', 'cssmin', 'nodemon']);
 }
